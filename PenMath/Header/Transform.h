b643acfb0ad142019b4ed5d2049d09bb4d4dbdcd
Malo <mal.sadoine@gmail.com>
2025-11-13 16:17:38 -0500

    [feat] : Create Transform Struct

---
+++ PenMath/Header/Transform.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "Vector/Vector3/Vector3.h"
+#include "Quaternion.h"
+#include "Matrix/Mat4.h"
+
+namespace PenMath
+{
+	struct Transform
+	{
+		PenMath::Vector3f position;
+		PenMath::Vector3f scale;
+		PenMath::Quaternion rotation;
+
+		_NODISCARD Mat4 toMatrix() const noexcept;
+
+		_NODISCARD Transform inverse() const;
+
+		_NODISCARD Transform combine(const Transform& otherTransform) const;
+
+		_NODISCARD bool operator==(const Transform& rvalue) const noexcept = default;
+		_NODISCARD bool operator!=(const Transform& rvalue) const noexcept = default;
+	};
+}
